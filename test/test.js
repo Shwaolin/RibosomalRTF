1890726a35da079e165fe2c91c8493dfd37cbdd6
rb_ <41527196+EndaHallahan@users.noreply.github.com>
2018-09-28 15:01:19 -0500

    Github commit

--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
 const RibosomalRTF = require("../index");
+const util = require('util');
 
 function test() {
-	RibosomalRTF.parseFile("./test/234.rtf").then(obj=>console.dir(obj));
-	
+	RibosomalRTF.parseFile("./test/234.rtf").then(obj => {
+		console.log(util.inspect(obj,{depth:null,colors:true,maxArrayLength:null,compact:false}));
+	});
 }
 
 test();
